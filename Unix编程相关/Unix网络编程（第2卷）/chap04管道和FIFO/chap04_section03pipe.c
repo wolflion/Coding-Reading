a3e6aaa324dfeb6b355055f2254f1a7897a28449
lionel Shen <successwolf@163.com>
2020-03-23 00:40:51 +0800

    pipe-main
    pipe()写了点，还没在树莓派上调试呢，先提交上去

---
+++ Unix编程相关/Unix网络编程（第2卷）/chap04管道和FIFO/chap04_section03pipe.c
@@ -0,0 +1,28 @@
+﻿#include "../unpipc_lionel.h"
+void client(int, int);
+void server(int, int);
+
+int main(int argc, char* argv[])
+{
+	int pipe1[2], pipe2[2];
+	pid_t childpid;
+
+	pipe(pipe1);
+	pipe(pipe2);
+
+	if ((childpid = fork()) == 0)//child
+	{
+		close(pipe1[1]);  //书上为何要用包裹函数Close()？没看第1篇呢
+		close(pipe2[0]);
+		server(pipe1[0], pipe2[1]);
+		exit(0);
+	}
+	//parent
+	close(pipe1[0]);
+	close(pipe2[1]);
+
+	client(pipe2[0], pipe1[1]);
+
+	waitpid(childpid, NULL, 0);//wait for child to terminate
+	exit(0);
+}
