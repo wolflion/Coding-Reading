6fa91709f0cce64daa5ab2bba943a1f9135ddce1
lionel Shen <successwolf@163.com>
2020-03-25 18:56:58 +0800

    mmap的使用
    并提交了一下这几天的md文件

---
+++ Unix编程相关/视频类的听讲/黑马第24期培训-02Linux系统课程/day06进程间通信/day06_13mmap.c
@@ -0,0 +1,37 @@
+﻿#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <string.h>
+
+int main()
+{
+	/*
+	如果是新创建一个文件
+	int fd = open("mem.txt",O_RDWR|O_CREAT|O_TRUNC,0664);
+	ftruncate(fd,8);
+	*/
+
+	int fd = open("mem.txt",O_RDWR);
+
+	//char *mem = mmap(NULL,8,PORT_READ|PORT_WRITE,MAP_SHARED,fd,0);
+	char *mem = mmap(NULL, 8, PORT_READ | PORT_WRITE, MAP_PRIVATE, fd, 0);
+	if (mem == MAP_FAILED)
+	{
+		perror("mmap err");
+		return -1;
+	}
+
+	//拷贝数据
+	strcpy(mem, "hello");
+
+	//释放
+	if (munmap(mem, 8) < 0)//8是length
+	{
+		perror("munmap err");
+	}
+	close(fd);
+	return 0;
+}
