54aa14208e1c85faf179bb3ef702dc69da596b04
lionel Shen <successwolf@163.com>
2020-04-15 21:57:03 +0800

    epoll服务端 伪代码和实现

---
+++ Unix编程相关/视频类的听讲/黑马第24期培训-03高并发网络编程开发/day13epoll-udp/day13_06epoll工作流程-伪代码.c
@@ -0,0 +1,72 @@
+﻿//先记住核心，具体用啥了，再补充定义
+
+int main()
+{
+	//创建监听的套接字
+	int lfd = socket();
+	//绑定
+	bind();
+	//监听
+	listen();
+
+	//epoll树根节点
+	int epfd = epoll_create(3000);
+
+	//存储发生变化的fd对应信息
+	struct epoll_event all[3000];
+
+	//初始化
+	//监听的lfd挂在epoll树上
+	struct epoll_event ev;
+
+	//在ev中init fd的信息
+	ev.events = EPOLLN;
+	ev.data.fd = lfd;
+
+	epoll_ctl(epfd, EPOLL_CTL_ADD, lfd, &ev);
+
+	//委托内核一直去检测事件
+	while (true)
+	{
+		//这里面要有efd，所以得在外面epoll_create()
+		int ret = epoll_wait(efd,all,3000,-1);  //ret就是返回多少个  【**这是个核心**】
+
+		//根据ret遍历all数组
+		for (int i = 0; i < ret;++i)
+		{
+			//两类文件描述符（有新的连接，已经连接的客户端有数据发送过来）
+			int fd = all[i].data.fd;
+			//有新的连接
+			if (fd == lfd)
+			{
+				//接收连接请求--accept不阻塞
+				int cfd = accept();
+				//cfd挂在树上
+				ev.events = EPOLLIN;
+				ev.data.fd = cfd;
+				epoll_ctl(epfd, EPOLL_CTL_ADD, cfd, &ev);
+			}
+			//已经连接的客户端有数据发送过来
+			else
+			{
+				//只处理客户端发来的数据
+				if (all[i].events &EPOLLIN) //in,out类型
+				{
+					//结果是0，跳过当次循环，比如人家是EPOLLOUT
+					continue;
+				}
+				//读数据
+				int len = recv();
+				if (len == 0)
+				{
+					//客户端关闭了连接
+					close(fd);
+					//检测的fd从树上删除
+					epoll_ctl(epfd, EPOLL_CTL_DEL, fd, NULL);
+				}
+				//写数据
+				send();
+			}
+		}
+	}
+}
---
+++ Unix编程相关/视频类的听讲/黑马第24期培训-03高并发网络编程开发/day13epoll-udp/day13_08epoll_server.c
@@ -0,0 +1,75 @@
+﻿//借助原先的代码，可以作为一个模板，单独写出来一下
+#include<sys/epoll.h>   //【K跳转】
+
+//创建epoll树根节点
+int epfd = epoll_create(2000);
+//初始化epoll树
+struct epoll_event ev;
+ev.events = EPOLLN;
+ev.data.fd = lfd;
+epoll_ctl(epfd, EPOLL_CTL_ADD, lfd, &ev);
+
+//存储发生变化的fd对应信息
+struct epoll_event all[2000];
+
+while (true)
+{
+	//使用epoll通知内核fd文件IO检测
+	//参数-1表示阻塞状态
+	int ret = epoll_wait(epfd,all,sizeof(all)/sizeof(all[0]),-1);
+
+	//遍历all数组中的前ret个元素
+	for (int i = 0; i < ret; ++i)
+	{
+		int fd = all[i].data.fd;
+		//判断是否有新连接
+		if (fd == lfd)
+		{
+			//接收连接请求--accept不阻塞
+			int cfd = accept(lfd,(struct sockaddr*)&client_addr,&cli_len);
+			if (cfd == -1)
+			{
+				perror("accept error");
+				exit(1);
+			}
+			//将新得到的cfd挂在树上
+			struct epoll_event temp;
+			temp.events = EPOLLIN;
+			temp.data.fd = cfd;
+			epoll_ctl(epfd, EPOLL_CTL_ADD, cfd, &temp);
+		}
+		//已经连接的客户端有数据发送过来
+		else
+		{
+			//处理已经连接的客户端发来的数据
+			if (!all[i].events &EPOLLIN) //in,out类型  【只处理读事件】
+			{
+				//结果是0，跳过当次循环，比如人家是EPOLLOUT
+				continue;
+			}
+			//读数据
+			char buf[1024] = { 0 };
+			int len = recv(fd,buf,sizeof(buf),0);
+			if (len == 0)
+			{
+				printf("client disconnected ...\n");
+				//客户端关闭了连接
+				close(fd);
+				//检测的fd从树上删除
+				epoll_ctl(epfd, EPOLL_CTL_DEL, fd, NULL);
+			}
+			else if (len == -1)
+			{
+				perror("recv error");
+				exit(1);
+			}
+			else
+			{
+				//写数据
+				printf("recv buf: %s\n", buf);
+				//send();
+				write(fd,buf,len);
+			}
+		}
+	}
+}
