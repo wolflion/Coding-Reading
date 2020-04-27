b234f8a60503a6db86268b1159bc180807bccc99
lionel Shen <successwolf@163.com>
2020-04-28 00:52:18 +0800

    C语言接口与实现-初学

---
+++ 99书&代码相关/2020/C语言接口与实现/chap01引言/文学程序.c
@@ -0,0 +1,39 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <ctype.h>
+
+int dotProduct(int x[], int y[], int n)
+{
+	int i, sum;
+	sum = 0;
+	for (i = 0; i < n;i++)
+	{
+		sum += x[i] * y[i];
+	}
+	return sum;
+}
+
+int main(int argc, char *argv[])
+{
+	int i;
+	for ( i = 1; i < argc; i++)
+	{
+		FILE *fp = fopen(argv[i], "r");
+		if (fp == NULL)
+		{
+			fprintf(stderr, "%s: can't open '%s' (%s)\n", argv[0], argv[1], strerror(errno));
+			return EXIT_FAILURE;
+		}
+		else
+		{
+			doubleword(argv[i], fp);
+			fclose(fp);
+		}
+	}
+	if (argc == 1)
+	{
+		doubleword(NULL, stdin);
+	}
+	return EXIT_FAILURE;
+}
