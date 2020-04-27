dafb4a0918f7be1d28a1e046114ed54a4d2cf6a8
lionel Shen <successwolf@163.com>
2020-04-28 00:53:20 +0800

    零基础学C++，第16章

---
+++ 99书&代码相关/2020/从零开始学C++程序设计/chap16异常处理与模板/chap16_01-除数是0的异常处理.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <cstdlib>
+unsigned namespace std;
+
+int main()
+{
+	cout << "==一个简单的例外范例==\n";
+	//使用try...catch来捕获异常
+	try
+	{
+		int n1;
+		cout << "请输入除数：";
+		cin >> n1;//请输入除数值
+		if (n1==0)
+		{
+			throw 1;  //question：如果没有throw 1呢
+		}
+		cout << "没有捕获到例外" << endl;
+	}
+	catch (int i)
+	{
+		cout << "捕获到除数为0的例外" << endl;
+	}
+	//question：如果上面不写throw 1;  这里写个 catch(...){}会不会被提示
+	cout << "结束程序的执行" << endl;
+
+	system("pause");
+	return 0;
+	//这是默认的catch
+#if 0
+	catch (CMemoryException* e)
+	{
+		
+	}
+	catch (CFileException* e)
+	{
+	}
+	catch (CException* e)
+	{
+	}
+#endif
+}
