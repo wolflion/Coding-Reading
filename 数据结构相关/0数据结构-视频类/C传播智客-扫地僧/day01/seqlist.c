c09e709b080baeb7561e3634d7142af3b4d35305
lionel Shen <successwolf@163.com>
2020-04-28 00:53:40 +0800

    传播智客-数据结构day01

---
+++ 数据结构相关/0数据结构-视频类/C传播智客-扫地僧/day01/seqlist.c
@@ -0,0 +1,94 @@
+﻿#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "seqlist.h"
+
+//在结构体中套1级指针
+typedef struct _tag_SeqList
+{
+	int length;
+	int capcity;
+	//int array[capcity];
+	unsigned int *node;
+}TSeqList;
+
+
+
+List* SeqList_Create(int capacity)  //SeqList
+{
+	int ret = 0;
+	TSeqList *tmp = NULL;
+	tmp = (TSeqList*)malloc(sizeof(TSeqList));
+	if (tm == NULL)
+	{
+		printf("func SeqList_Create() err: %d \n",ret);
+		return -1;
+	}
+
+	memset(tmp, 0, sizeof(TSeqList);
+
+	//根据capacity的大小分配节点的空间
+	tmp->node = (unsigned int *)malloc(sizeof(unsigned int *)*capacity);
+	if (tmp->node == NULL)
+	{
+		ret = -2;
+		printf("func SeqList_Create() err: malloc err %d\n", ret);
+		return NULL;
+	}
+
+	tmp->capcity = capacity;
+	tmp->length = 0;
+
+	return NULL;
+}
+
+void SeqList_Destroy(List* list)
+{
+	TSeqList *tlist = NULL;
+	if (list == NULL)
+	{
+		return;
+	}
+	tlist = (TSeqList *)list;
+	if (tlist->node != NULL)
+	{
+		free(tlist->node);
+	}
+	free(tlist);
+	return;
+}
+
+//清空链表，就是把长度置0，回到初始化情况
+void SeqList_Clear(List* list)
+{
+	TSeqList *tlist = NULL;
+	if (list == NULL)
+	{
+		return;
+	}
+	tlist = (TSeqList *)list;
+	tlist->length = 0;
+	return;
+}
+
+int SeqList_Length(List *list)
+{
+	TSeqList *tlist = NULL;
+	if (list == NULL)
+	{
+		return -1;
+	}
+	tlist = (TSeqList *)list;
+	return tlist->length;
+}
+
+int SeqList_Capacity(List *list)
+{
+	TSeqList *tlist = NULL;
+	if (list == NULL)
+	{
+		return -1;
+	}
+	tlist = (TSeqList *)list;
+	return tlist->capcity;
+}
---
+++ 数据结构相关/0数据结构-视频类/C传播智客-扫地僧/day01/seqlist.h
@@ -0,0 +1,34 @@
+﻿/*
+C璇█鎻忚堪=====銆嬬嚎鎬ц〃鐨勮璁′笌瀹炵幇
+ADT鎶借薄灞? 銆奫鏁版嵁缁撴瀯(C璇█鐗?].涓ヨ敋鏁廮鍚翠紵姘?鎵弿鐗?pdf銆?p44椤?
+浜虹敓璐㈠瘜搴撶Н绱?
+*/
+ 
+#ifndef _WBM_LIST_H_
+#define _WBM_LIST_H_
+ 
+typedef void List;
+typedef void ListNode;
+ 
+//鍒涘缓骞朵笖杩斿洖涓€涓┖鐨勭嚎鎬ц〃
+List* List_Create();
+ 
+//閿€姣佷竴涓嚎鎬ц〃list
+void List_Destroy(List* list);
+ 
+//灏嗕竴涓嚎鎬ц〃list涓殑鎵€鏈夊厓绱犳竻绌? 绾挎€ц〃鍥炲埌鍒涘缓鏃剁殑鍒濆鐘舵€?
+void List_Clear(List* list);
+ 
+//杩斿洖涓€涓嚎鎬ц〃list涓殑鎵€鏈夊厓绱犱釜鏁?
+int List_Length(List* list);
+ 
+//鍚戜竴涓嚎鎬ц〃list鐨刾os浣嶇疆澶勬彃鍏ユ柊鍏冪礌node
+int List_Insert(List* list, ListNode* node, int pos);  
+ 
+//鑾峰彇涓€涓嚎鎬ц〃list鐨刾os浣嶇疆澶勭殑鍏冪礌
+ListNode* List_Get(List* list, int pos);
+ 
+//鍒犻櫎涓€涓嚎鎬ц〃list鐨刾os浣嶇疆澶勭殑鍏冪礌  杩斿洖鍊间负琚垹闄ょ殑鍏冪礌锛孨ULL琛ㄧず鍒犻櫎澶辫触
+ListNode* List_Delete(List* list, int pos);
+ 
+#endif
