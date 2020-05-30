127c25545c217daba82b7094e000cabd1cd7cc67
lionel Shen <successwolf@163.com>
2020-05-30 11:42:56 +0800

    郝斌DS-两个复习知识

---
+++ 数据结构相关/0数据结构-视频类/H郝斌78讲/33栈程序演示.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
+
+typedef struct Node   //这是元素
+{
+	int data;
+	struct Node *pNext;
+}NODE,*PNODE;
+
+typedef struct Stack
+{
+	PNODE pTop;  //**pTop永远指向顶端元素**
+	PNODE pBottom;  //pBottom位置一直不变
+}STACK, *PSTACK;
+
+int main()
+{
+
+	//栈空：top==bottom;
+	//栈满：没有栈满的情况
+
+	STACK s;//等价于struct Stack s;
+	
+	initStack();//初始化Stack
+	pushStack();//放元素，压栈
+
+	return 0;
+}
