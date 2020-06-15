fba106196ae4bb4ce1cc34071c32fb54dc2df23b
lionel Shen <successwolf@163.com>
2020-06-15 21:08:13 +0800

    传播智客C++，day07

---
+++ 44视频类/C++基础（传播智客）/day07/02多态原理探究.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+using namespace std;
+
+class Parent
+{
+public:
+	Parent(int a = 0)
+	{
+		this->a = a;
+	}
+	//动手脚1，virtual
+	virtual void print()  //要虚函数
+	{
+		cout << "Parent" << endl;
+	}
+private:
+	int a;
+};
+
+class Child:public Parent(
+{
+public:
+	Child(int a = 0, int b = 0) :Parent(a)
+	{
+		this->b = b;
+	}
+	virtual void print() //子类，可加可不加
+	{
+		cout << "Child" << endl;
+	}
+private:
+	int b;
+};
+
+void HowToPlay(Parent *base)
+{
+	//动手脚2
+	base->print();//会有多态发生
+}
+
+int main()
+{
+	Parent p1; //动手脚3  【**对象模型初探**】，C++会增加一个vptr指针	
+	Child c1;
+	HowToPlay(&p1);
+	HowToPlay(&c1);
+
+	system("pause");
+	return 0;
+}
---
+++ 44视频类/C++基础（传播智客）/day07/03证明vptr指针的存在.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+class Parent1
+{
+public:
+	Parent1(int a = 0)
+	{
+		this->a = a;
+	}
+	virtual void print()
+	{
+		cout << "Parent" << endl;
+	}
+private:
+	int a;
+};
+
+class Parent2
+{
+public:
+	Parent2(int a = 0)
+	{
+		this->a = a;
+	}
+	void print()
+	{
+		cout << "Parent" << endl;
+	}
+private:
+	int a;
+};
+
+int main()
+{
+	cout << sizeof(Parent1) << endl; //8
+	cout << sizeof(Parent2) << endl; //4
+	system("pause");
+	return 0;
+}
