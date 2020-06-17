f083449d99f0bad36a197a2b8797cdd7c4ba87bb
lionel Shen <successwolf@163.com>
2020-06-17 22:21:16 +0800

    C++基础 day01，三目运算符在C和C++中的不同表现

---
+++ 44视频类/C++基础（传播智客）/day01/15三目运算符.c
@@ -0,0 +1,11 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int a = 10;
+	int b = 20;
+
+	(a < b ? a : b) = 30; //C语言里，表达式不能做左值
+	printf("a=%d,b=%d\n",a,b);
+	return 0;
+}
---
+++ 44视频类/C++基础（传播智客）/day01/15三目运算符.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int a = 10;
+	int b = 20;
+
+	(a < b ? a : b) = 30; //C语言里，表达式不能做左值
+	printf("a=%d,b=%d\n", a, b);
+
+	system("pause");
+	return 0;
+}
