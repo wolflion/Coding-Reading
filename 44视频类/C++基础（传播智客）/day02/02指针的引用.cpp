ebe2b178d24bd5b91edfd009c218647be8e138fb
lionel Shen <successwolf@163.com>
2020-06-21 22:04:21 +0800

    C++基础 - day02 学习曲线

---
+++ 44视频类/C++基础（传播智客）/day02/02指针的引用.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+//
+struct Teacher
+{
+	char name[64];
+	int age;
+};
+
+//在被调用函数，获取资源
+void getTeacher(Teacher **p)
+{
+	Teacher *tmp = NULL;
+	if (p == NULL)
+	{
+		return -1;
+	}
+	tmp = (Teacher *)malloc(sizeof(Teacher);
+	if (tmp == NULL)
+	{
+		return -1;
+	}
+	tmp->age = 33;
+	//p是实参的地址，*实参的地址，去间接的修改实参的值。
+	*p = tmp;
+}
+
+void main()
+{
+	Teacher *pT1 = NULL;
+
+	getTeacher(&pT1);//二级指针
+
+	cout << "age" << pT1->age << endl;
+
+	cout << "hello..." << endl;
+	system("pause");
+}
