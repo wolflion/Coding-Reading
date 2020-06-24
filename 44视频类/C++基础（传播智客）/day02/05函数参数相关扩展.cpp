c1b91b7621b8ddb4f6b73a434c6926569ff82a79
lionel Shen <successwolf@163.com>
2020-06-24 21:22:20 +0800

    C++基础-day02-函数重载，默认参数

---
+++ 44视频类/C++基础（传播智客）/day02/05函数参数相关扩展.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+//1、函数默认参数
+//void myPrint1(int y, int x=3){}
+//void myPrint2(int x=3,int y){}  //这种情况，会编译报错
+void myPrint(int x = 3)//多个默认时，最右边必须要有默认参数
+{
+	cout << "x=" << x << endl;
+}
+
+//2、函数占位参数
+void func1(int a, int b, int)
+{
+	cout << "a" << a << "b" << b << endl;
+}
+
+//默认参数和占位参数 结合使用
+void func2(int a, int b, int = 0)
+{
+	cout << "a" << a << "b" << b << endl;
+}
+
+int main(int argc, char** argv)
+{
+	myPrint(4);
+	myPrint();
+
+	//func1(1, 2);  //这种情况会报错
+	func1(1, 2, 3);
+
+	//结合使用时，都没有错误
+	func2(1, 2);
+	func2(1, 2, 3);
+	getchar();
+	return 0;
+}
+
---
+++ 44视频类/C++基础（传播智客）/day02/06函数重载.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+using namespace std;
+
+void myPrint(int a)
+{
+	cout << a << endl;
+}
+
+void myPrint(char *p)
+{
+	cout << *p << endl;
+}
+
+void myPrint(int a, int b)
+{
+	cout << a << b<<endl;
+}
+
+/*
+int myPrint(int a, int b)  //只是返回值类型不一样，不是函数重载，编译报错
+{
+	cout << a << b << endl;
+}
+*/
+
+
+int add(int a, int b, int c = 0)
+{
+	return a + b + c;
+}
+
+int add(int a, int b)
+{
+	return a + b;
+}
+
+int main(int argc, char** argv)
+{
+	myPrint(1);
+	myPrint("ab");
+	myPrint(1, 2);
+
+	//这个会有二义性，编译器不知道调用谁
+	//add(3, 4);
+
+	getchar();
+	return 0;
+}
+
