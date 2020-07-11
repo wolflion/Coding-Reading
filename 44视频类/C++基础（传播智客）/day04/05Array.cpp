1020fbed4972586ed9ef9a2f1fad8e944f01f48f
lionel Shen <successwolf@163.com>
2020-07-11 14:15:27 +0800

    C++day04的05

---
+++ 44视频类/C++基础（传播智客）/day04/05Array.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+using namespace std;
+#include "05Array.h"
+
+int main()
+{
+	Array a1(10);
+	for (int i = 0; i < a1.length();i++)
+	{
+		a1.setData(i, i);
+	}
+
+	for (int i = 0; i < a1.length();i++)
+	{
+		printf("array %d:%d\n", i, a1.getData(i));
+	}
+
+	Array a2=a1;
+
+	for (int i = 0; i < a2.length(); i++)
+	{
+		printf("array %d:%d\n", i, a2.getData(i));
+	}
+
+	cin.get();
+	//system("pause");
+	return 0;
+}
+
+//不写后面的实现，链接就找不到；---lionel 20200711
+
+Array::Array(int length)
+{
+
+}
+Array::Array(const Array &obj)
+{
+
+}
+int Array::length()
+{
+	return 0;
+}
+void Array::setData(int index, int value)
+{
+
+}
+int Array::getData(int index)
+{
+	return 0;
+}
+
+Array::~Array()
+{
+
+}
---
+++ 44视频类/C++基础（传播智客）/day04/05Array.h
@@ -0,0 +1,21 @@
+﻿#ifndef _MYARRAY_H
+#define _MYARRAY_H
+
+#pragma once
+
+class Array
+{
+public:
+	Array(int length);
+	Array(const Array &obj);
+	int length();
+	void setData(int index, int value);
+	int getData(int index);
+	~Array();
+protected:
+private:
+	int mLength;
+	int* mSpace;
+};
+
+#endif
