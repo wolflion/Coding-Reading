572a7fa2a740c883be2c10b4876c23ec0238507e
lionel Shen <successwolf@163.com>
2020-08-02 23:58:34 +0800

    C++并发编程chap01部分

---
+++ 99书&代码相关/2020/C++并发编程实战/C++并发编程实战.md
@@ -0,0 +1,5 @@
+### chap01、你好，C++并发世界
+
+#### 1.4、开始入门
+
++ 1-01.cpp 一个简单的Hello,Concurrent Word程序。
---
+++ 99书&代码相关/2020/C++并发编程实战/chap01你好，C++并发世界/1-01Hello_Concurrent_World.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <thread>
+
+void hello()
+{
+    std::cout<<"Hello Concurrent World\n";
+}
+
+int main()
+{
+    std::thread t(hello);
+    t.join();
+}
+
+// CLion编译成功，输出  Hello Concurrent World
