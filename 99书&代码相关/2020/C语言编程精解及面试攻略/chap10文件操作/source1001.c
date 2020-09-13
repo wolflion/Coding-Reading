c44c1c2d57902b4aa7734813c1a44496752d7de7
lionel Shen <successwolf@163.com>
2020-09-13 20:29:26 +0800

    chap10文件操作，部分

---
+++ 99书&代码相关/2020/C语言编程精解及面试攻略/chap10文件操作/source1001.c
@@ -0,0 +1,24 @@
+﻿// 程序1：将一组字符存储到一个文本文件中
+// 将一组字符存储到一个文本文件中
+#include <stdio.h>
+int main()
+{
+	char ch;
+
+	// 声明文件指针
+	FILE *fp;
+
+	// 打开文件用于写入数据
+	fp = fopen("myfile.txt", "w");
+
+	// 提示输入数据
+	printf("\nEnter text: \n");
+
+	while ((ch = getchar())!=EOF)
+	{
+		putc(ch, fp);
+	}
+
+	// 关闭文件
+	fclose(fp);
+}
---
+++ 99书&代码相关/2020/C语言编程精解及面试攻略/chap10文件操作/source1002.c
@@ -0,0 +1,20 @@
+﻿// 程序2：从一个文本文件中读入文本
+// 读取并显示一个文本文件
+#include <stdio.h>
+int main()
+{
+	char ch;
+
+	// 创建一个文件指针用于读入一个文本文件
+	FILE *fp = fopen("myfile.txt", "r");
+
+	// 逐字符读入文本并显示
+	printf("\nThe file contents: \n");
+	while ((ch = getc(fp))!=EOF)
+	{
+		putchar(ch);
+	}
+
+	// 关闭文件
+	fclose(fp);
+}
