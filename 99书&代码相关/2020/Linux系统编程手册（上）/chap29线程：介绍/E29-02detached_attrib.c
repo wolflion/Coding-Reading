91984004fa9a32c16c37f95cc096281b31ed6109
lionel Shen <successwolf@163.com>
2020-10-01 01:06:27 +0800

    chap29线程：介绍

---
+++ 99书&代码相关/2020/Linux系统编程手册（上）/chap29线程：介绍/E29-02detached_attrib.c
@@ -0,0 +1,19 @@
+﻿pthread_t thr;
+pthread_attr_t attr;
+int s;
+
+s = pthread_attr_init(&attr);  // Assigns default values
+if (s != 0)
+    ;
+
+s = pthread_attr_setdetachstate(&attr, PTHRED_CREATE_DETACHED);
+if (s != 0)
+;
+
+S = pthread_create(&thr, &attr, thread_Func, (void*)1);
+if (s != 0)
+;
+
+s = pthread_attr_destroy(&attr);   // No longer needed
+if (s != 0)
+;
