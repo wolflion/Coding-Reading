b228b3c7ffb98994cce29b04f50cf3b717066313
lionel Shen <successwolf@163.com>
2020-10-25 22:42:48 +0800

    编程剖析01

---
+++ 99书&代码相关/2020/C++编程剖析/P03类的设计、继承和多态/Item14顺序、顺序/01初级问题.cpp
@@ -0,0 +1,22 @@
+﻿#include <string>
+using namespace std;
+
+class A
+{
+public:
+	A(const string &s){ ; }
+	string f(){ return "hello world"; }
+};
+
+class B : public A
+{
+public:
+	B() :A(s = f()){}  // 这行有问题。
+private:
+	string s;
+};
+
+int main()
+{
+	B b;
+}
