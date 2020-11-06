14910a77683ef36a72a4aa0f734768bace86ecf7
lionel Shen <successwolf@163.com>
2020-11-06 23:49:46 +0800

    04_09更新完成

--- 99书&代码相关/2020/C++泛型STL原理和应用/chap04模拟STL三大件/04_09.cpp
+++ 99书&代码相关/2020/C++泛型STL原理和应用/chap04模拟STL三大件/04_09.cpp
@@ -33,4 +33,46 @@ int main()
 	for_each(vec_A.begin(), vec_A.end(), [](char c) {cout << c << " "; });
 	cout << endl;
 	cout << endl;
+
+	cout << "删除数据元素" << endl;
+	vec_A.erase(it, vec_A.end());
+	for_each(vec_A.begin(), vec_A.end(), [](char c) {cout << c << " "; });
+	cout << endl;
+	cout << endl;
+
+	cout << "用字符c替换数据元素B:" << endl;
+	// error "Expression: cannot seek vector iterator after end"
+	//replace(vec_A.begin(), vec_A.end() + 2, 'B', 'C');
+	replace(vec_A.begin(), vec_A.begin() + 2, 'B', 'C'); // 这里写end时，会越界错
+	replace(vec_B.begin(), vec_B.end(), 'B', 'X');
+	for_each(vec_A.begin(), vec_A.end(), [](char c) {cout << c << " "; });
+	cout << endl;
+	cout << endl;
+
+	cout << "插入数据元素:" << endl;
+	vec_A.insert(vec_A.begin(), 'D');
+	vec_A.insert(vec_A.end(), 'A');
+	for_each(vec_A.begin(), vec_A.end(), [](char c) {cout << c << " "; });
+	cout << endl;
+	cout << endl;
+
+	cout << "排序" << endl;
+	sort(vec_A.begin(), vec_A.end());
+	for_each(vec_A.begin(), vec_A.end(), [](char c) {cout << c << " "; });
+	cout << endl;
+	cout << endl;
+
+	cout << "定义vec_C并使其大小等于vec_A.size()+vec_B:" << endl;
+	vector<char>vec_C(vec_A.size() + vec_B.size());
+
+	cout << "vec_A和vec_B合并输入vec_C:" << endl;
+	merge(vec_A.begin(), vec_A.end(), vec_B.begin(), vec_B.end(), vec_C.begin());
+	for_each(vec_A.begin(), vec_A.end(), [](char c) {cout << c << " "; });
+	cout << endl;
+	cout << endl;
+
+	cout << "统计vec_C中字符B的数目:" << endl;
+	num = count(vec_C.begin(), vec_C.end(), 'B');
+	cout << num << endl;
+	return 0;
 }
